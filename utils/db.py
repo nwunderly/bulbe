397355247587a9b9984ba3ace586e5d828e3b058
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-01-07 16:00:38 -0500

    start ORM setup with SQLAlchemy

--- utils/db.py
+++ utils/db.py
@@ -1,8 +1,16 @@
 import databases
 import logging
+import sqlalchemy as sql
+
+from typing import List
 
 
 logger = logging.getLogger('utils.db')
+
+
+############
+# DATABASE #
+############
 
 
 class Database:
@@ -33,25 +41,136 @@ class Database:
         return self.conn.is_connected
 
 
-#########################################################################
+################
+# DATA CLASSES #
+################
+
+
+class Row:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def from_record(cls, record):
+        row = cls()
+        for name, value in record.values():
+            row.__setattr__(name, value)
+        return row
+
+
+class GuildConfig(Row):
+    pass
+
+
+#############
+# ORM STUFF #
+#############
+
+
+guild_config = sql.Table(
+    'guild_config',
+    sql.MetaData(),
+    sql.Column('guild_id', sql.BIGINT, primary_key=True),
+    sql.Column('muted_role', sql.BIGINT),
+    sql.Column('admin_role', sql.BIGINT),
+    sql.Column('mod_role', sql.BIGINT),
+    sql.Column('ignored_users', sql.ARRAY(sql.BIGINT)),
+    sql.Column('ignored_roles', sql.ARRAY(sql.BIGINT)),
+    sql.Column('ignored_channels', sql.ARRAY(sql.BIGINT)),
+    sql.Column('autoroles_user', sql.ARRAY(sql.BIGINT)),
+    sql.Column('autoroles_bot', sql.ARRAY(sql.BIGINT)),
+    sql.Column('role_persist', sql.ARRAY(sql.BIGINT)),
+    sql.Column('modlog_channel', sql.BIGINT),
+    sql.Column('prefix', sql.TEXT),
+)
+
+
+############
+# TABLES #
+############
 
 
 class Table:
     def __init__(self, name, db: Database):
         self.name = name
         self.db = db
+        self.conn = db.conn
+        self.cache = {}
+
+    @staticmethod
+    def where_expr(**checks):
+        exprs = []
+        values = {}
+        for var, value in checks.items():
+            exprs.append(f"{var} = :{var}")
+            values[var] = value
+        where_expr = " AND ".join(exprs)
+        return where_expr, values
+
+    async def fetch_row(self, **checks):
+        where_expr, values = self.where_expr(**checks)
+        query = f"SELECT * FROM {self.name} WHERE {where_expr}"
+        return await self.conn.fetch_one(query, values)
+
+    async def fetch_row_cached(self, **primary_key_checks):
+        primary_key = list(primary_key_checks.keys())
+
+        if primary_key in self.cache:
+            return self.cache[primary_key]
+
+        return await self.fetch_row(**primary_key_checks)
 
 
 class ConfigTable(Table):
+    ROWS = {
+        'guild_id': int,
+        'prefix': str,
+        'muted_role': int,
+        'admin_role': int,
+        'mod_role': int,
+        'ignored_users': List[int],
+        'ignored_roles': List[int],
+        'ignored_channels': List[int],
+        'autoroles_user': List[int],
+        'autoroles_bot': List[int],
+        'role_persist': List[int],
+        'modlog_channel': List[int],
+    }
+
     def __init__(self, db):
         super().__init__('config', db)
 
+    async def exists(self, guild_id):
+        query = r"SELECT exists(SELECT 1 FROM config WHERE guild_id = $1)"
+        exists = await self.conn.fetch_val(query, guild_id)
+        return exists
+
     async def new_guild_config(self, guild_id):
-        query = r"INSERT INTO config " \
-                r"(guild_id, prefix, muted_role, admin_role, mod_role, ignored_users, ignored_roles, ignored_channels, " \
-                r"autoroles_user, autoroles_bot, role_persist, modlog_channel) VALUES " \
-                r"($1, '+', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)"
-        await self.db.conn.execute(query, guild_id)
+        query = r"INSERT INTO config (guild_id) VALUES (:guild_id) RETURNING *"
+        row = await self.conn.fetch_one(query, {'guild_id': guild_id})
+        config = GuildConfig.from_record(row)
+        self.cache[guild_id] = config
+        return config
+
+    async def get_guild_config(self, guild_id):
+        if guild_id in self.cache:
+            return self.cache[guild_id]
+
+        query = r"SELECT * FROM config WHERE guild_id = :guild_id"
+        row = await self.conn.fetch_one(query, {'guild_id': guild_id})
+        config = GuildConfig.from_record(row)
+        self.cache[guild_id] = config
+        return config
+
+    async def set_prefix(self, guild_id, prefix):
+        if not await self.exists(guild_id):
+            await self.new_guild_config(guild_id)
+        query = r"UPDATE config SET prefix = :prefix WHERE guild_id = :guild_id RETURNING *"
+        row = await self.conn.fetch_one(query, {'guild_id': guild_id, 'prefix': prefix})
+        config = GuildConfig.from_record(row)
+        self.cache[guild_id] = config
+        return config
+
 
 
 class InfractionsTable(Table):
